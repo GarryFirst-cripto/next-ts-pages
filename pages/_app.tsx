74f16fa9e14226ca76ad9b2b52a515cbb16beae1
GarryFirst-cripto <vist_01@ukr.net>
2024-06-08 14:16:21 +0300

    test_01

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,37 @@
 import { AppProps } from "next/app";
+import { useEffect } from 'react'
 import Layout from "./components/layout";
 import './styles/globals.scss';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <main>
-      <Component {...pageProps} />
-    </main>
-  </Layout>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  useEffect(() => {
+
+    const originalFetch = window.fetch
+
+    window.fetch = async (input: RequestInfo | URL, init: RequestInit = {}): Promise<Response> => {
+      const token = 'qqqqqqqqqqqwwwwwwwwwwweeeeeeeeerrrrrrrr'; //localStorage.getItem('token')
+
+      localStorage.setItem('token', token)
+      // localStorage.removeItem('token')
+
+      init.headers = init.headers || {}
+
+      if (token) {
+        (init.headers as Record<string, string>)['Authorization'] = `Bearer ${token}`
+      }
+console.log('EEEEEEEEEEE', input);
+      return originalFetch(input, init)
+    }
+  }, [])
+
+  return (
+    <Layout>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
+}
 
 export default MyApp;
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,36 +2,59 @@ import Head from "next/head";
 import Link from "next/link";
 import Heading from "../components/heading";
 import { IPost } from '../components/postInfo';
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 
-export const getStaticProps = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await response.json();
+// export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
 
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
+//   console.log('YYYYYYYYY', context)
 
-  return {
-    props: { posts: data },
-  }
-};
+//   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await response.json();
+
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+
+//   return {
+//     props: { posts: data },
+//   }
+// };
 
 const Posts = ({ posts }: { posts: [IPost] }) => { 
+  const [logg, setLogg] = useState(false);
+
+  const router = useRouter()
+console.log('XXXXXXXXXX')
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+console.log('TTTTTT', token);
+    if (!token) {
+      router.push('/')
+    } else {
+      setLogg(true);
+    }
+  }, [router])
+
   return (
     <>
-      <Head>
-        <title>Posts</title>
-      </Head>
-      <Heading text="Posts list:" />
-      <ul>
-        {posts && posts.map(({ id, title }) => (
-          <li key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-          </li>
-        ))}
-      </ul>
+      { logg && <>
+          <Head>
+            <title>Posts</title>
+          </Head>
+          <Heading text="Posts list:" />
+          <ul>
+            {posts && posts.map(({ id, title }) => (
+              <li key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      }
     </>
   );
 };
