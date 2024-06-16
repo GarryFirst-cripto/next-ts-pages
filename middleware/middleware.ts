1ef0882fc1889d2e53fef902a67c6d4ac19e8468
GarryFirst-cripto <vist_01@ukr.net>
2024-06-16 17:46:26 +0300

    stage_middleware_03

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -4,6 +4,7 @@ export function withMiddleware(handler: NextApiHandler) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     // Здесь можно добавить логику middleware
     const token = req.headers.authorization
+    
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' })
     }
